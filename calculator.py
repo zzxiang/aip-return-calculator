12e072ef06b66c524bbb47644f8f338cd6f29ec3
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2020-06-20 14:42:47 +0900

    函数改名

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def calc_final_ret_amount(invest_each_time, years, annual_ret, interval_cnt_a_year):
+def calc_final_asset(invest_each_time, years, annual_ret, interval_cnt_a_year):
     interval_net_value = pow(annual_ret + 1, 1 / interval_cnt_a_year)
     interval_cnt_total = years * interval_cnt_a_year
     final_net_value = \
@@ -9,5 +9,5 @@ def calc_final_ret_amount(invest_each_time, years, annual_ret, interval_cnt_a_ye
     final_ret_amount = invest_each_time * final_net_value
     return round(final_ret_amount, 2)
 
-print(calc_final_ret_amount(300, 3, 0.1, 12))
+print(calc_final_asset(300, 3, 0.1, 12))
 
